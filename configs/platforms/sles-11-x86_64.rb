f1d677031c02e3fa926d093b87a68d2065cd1b2e
Amit Karsale <amit.karsale@perforce.com>
2024-02-13 14:52:11 +0530

    (PA-6002) Add sles11-x86_64 platform definition to pxp-agent-vanagon-7.x

---
+++ configs/platforms/sles-11-x86_64.rb
@@ -0,0 +1,15 @@
+platform "sles-11-x86_64" do |plat|
+  plat.servicedir "/etc/init.d"
+  plat.defaultdir "/etc/sysconfig"
+  plat.servicetype "sysv"
+  plat.add_build_repository "http://osmirror.delivery.puppetlabs.net/sles-11-deps-x86_64/sles-11-deps-x86_64.repo"
+  plat.add_build_repository "http://pl-build-tools.delivery.puppetlabs.net/yum/sles/11/x86_64/pl-build-tools-sles-11-x86_64.repo"
+  packages = %w[
+    aaa_base autoconf automake libbz2-devel make pkgconfig pl-cmake readline-devel rsync zlib-devel
+  ]
+  plat.provision_with("zypper -n --no-gpg-checks install -y #{packages.join(' ')}")
+  plat.provision_with "zypper install -y --oldpackage pl-gcc=4.8.2-1"
+  plat.provision_with "zypper install -y --oldpackage pl-cmake-3.2.3-13.sles11.x86_64"
+  plat.install_build_dependencies_with "zypper -n --no-gpg-checks install -y"
+  plat.vmpooler_template "sles-11-x86_64"
+end
